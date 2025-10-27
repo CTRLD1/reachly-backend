92bd45b35e7fde9cf85bba12643278f31787cf48
dansubaie2 <dansubaie029@gmail.com>
2025-10-27 16:53:27 +0300

    created a new file for serializers in main_app and added a serializer for each of my models to start creating the views

---
+++ main_app/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Challenge, UserChallenge, Reflection
+
+# ref: cat-collector classwork code
+
+class ChallengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Challenge
+        fields = '__all__'
+
+class UserChallengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserChallenge
+        fields = '__all__'
+
+class ReflectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reflection
+        fields = '__all__'
