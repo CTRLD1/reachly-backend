b28d5c07c98f01a023f1901288e5c0bc7dec1217
dansubaie2 <dansubaie029@gmail.com>
2025-10-27 10:19:45 +0300

    Added the first model: Challenge with its fields and a str method. and registered it in admin.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+
+    def __str__(self):
+        return {self.title}
+
+
