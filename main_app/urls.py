801f93af363d5472a2792d2c03ef0637ff850b4d
dansubaie2 <dansubaie029@gmail.com>
2025-10-27 17:40:20 +0300

    Added ChallengeIndex view with a get method and added a path to it + tested the endpoint in postman

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, ChallengeIndex
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('challenges/', ChallengeIndex.as_view(), name='challenges_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Challenge
+from .serializers import ChallengeSerializer
 
 # Create your views here.
 
@@ -9,3 +13,15 @@ class Home(APIView):
     def get(self, request):
         content = {'message': 'helloo welcome to reachly API🙋🏽'}
         return Response(content)
+    
+# Challenge model (full CRUD)
+class ChallengeIndex(APIView):
+    def get(self, request):
+        try:
+            # to get all of the challenges from the db
+            queryset = Challenge.objects.all()
+            serializer = ChallengeSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        except Exception as err:
+            return Response({'error' : str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
